cc4370cf791f362c85925b6b38c6ed561b4c3646
mohamedtolba19 <mohamedtolba9911@gmail.com>
2026-05-15 18:09:24 +0300

    change password done

--- src/api/modules/auth.ts
+++ src/api/modules/auth.ts
@@ -1,9 +1,13 @@
+import type { ChangePassFormValues } from "../../modules/Authentication/components/ChangePassword/ChangePassword"
 import type { LoginFormValues } from "../../modules/Authentication/components/Login/Login"
 import type { VerifyFormValues } from "../../modules/Authentication/components/VerifyAccount/VerifyAccount"
 import axiosClient from "../axiosClient"
 
 export const Login = (data:LoginFormValues)=>{
     return axiosClient.post('/users/login' , data)
+}
+export const changePassword = (data:ChangePassFormValues)=>{
+    return axiosClient.put('/users/ChangePassword' , data)
 }
 
 export const VerifyAccount = (data:VerifyFormValues)=>{
