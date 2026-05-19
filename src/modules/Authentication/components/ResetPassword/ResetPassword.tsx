aa3fda7cb1e012cff3bd0ca535646b4243158866
Mohamed Elhussainey <mw61887@gmail.com>
2026-05-19 13:45:37 +0300

    making some updates on resetPassword UI

--- src/modules/Authentication/components/ResetPassword/ResetPassword.tsx
+++ src/modules/Authentication/components/ResetPassword/ResetPassword.tsx
@@ -1,7 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useForm } from "react-hook-form"
 import { AuthAPI } from "../../../../api"
 import { toast } from "react-toastify"
 import { useNavigate } from "react-router-dom"
+import { useState } from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons"
 
 export interface ResetPassFormValues {
   email: string,
@@ -12,7 +16,13 @@ export interface ResetPassFormValues {
 export default function ResetPassword() {
   const { register, handleSubmit, watch, formState: { errors } } = useForm<ResetPassFormValues>()
   const navigate = useNavigate()
+  const [showNewPassword, setShowNewPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+
+
   const onSubmit = async (data: ResetPassFormValues) => {
+    setIsLoading(true)
     try {
       const response = await AuthAPI.ResetPass(data)
       console.log(response);
@@ -23,6 +33,8 @@ export default function ResetPassword() {
       const err = error as any;
       const errorMessage = err?.response?.data?.message || err?.message || "Something went wrong";
       toast.error(errorMessage);
+    }finally{
+      setIsLoading(false);
     }
   }
   return (
@@ -31,7 +43,7 @@ export default function ResetPassword() {
         <h3>Reset Password</h3>
       </div>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="mt-4 d-flex flex-column form-input">
+        <div className="mt-4 d-flex flex-column form-input auth-form-input">
           <label htmlFor="email">E-mail</label>
           <input
             type="email"
@@ -49,7 +61,7 @@ export default function ResetPassword() {
         </div>
         {errors.email && <span className="text-danger">{errors.email.message}</span>}
 
-        <div className="mt-2 d-flex flex-column form-input">
+        <div className="my-3 d-flex flex-column form-input auth-form-input">
           <label htmlFor="code">OTP Verification</label>
           <input
             type="text"
@@ -62,40 +74,70 @@ export default function ResetPassword() {
           />
         </div>
         {errors.seed && <span className="text-danger">{errors.seed.message}</span>}
-        <div className="mt-2 d-flex flex-column form-input">
+        <div className="my-3 form-input auth-form-input">
           <label htmlFor="password">New Password</label>
-          <input
-            type="password"
-            className="form-control px-0 auth-input"
-            aria-describedby="passwordlpBlock"
-            placeholder="Enter your New Password"
-            {...register('password', {
-              required: 'Password is required!',
-            })}
-          />
+          <div className="input-group">
+            <input
+              type={showNewPassword ? "text" : "password"}
+              className="form-control px-0 auth-input"
+              placeholder="Enter your New Password"
+              {...register('password', {
+                required: 'Password is required!',
+              })}
+            />
+            <span
+              className="input-group-text bg-transparent border-0"
+              onClick={() => setShowNewPassword(!showNewPassword)}
+              style={{ cursor: "pointer" }}
+            >
+              <FontAwesomeIcon
+                color="#FFF"
+                icon={showNewPassword ? faEyeSlash : faEye}
+              />
+            </span>
+          </div>
         </div>
         {errors.password && <span className="text-danger">{errors.password.message}</span>}
 
-        <div className="mt-2 d-flex flex-column form-input">
-          <label htmlFor="confirmPassword">Confirm New Password</label>
-          <input
-            type="password"
-            className="form-control px-0 auth-input"
-            aria-describedby="confirmPasswordlpBlock"
-            placeholder="Confirm New Password"
-            {...register('confirmPassword', {
+        <div className="my-3 form-input auth-form-input">
+          <label htmlFor="confirmPassword">New Password</label>
+          <div className="input-group">
+            <input
+              type={showNewPassword ? "text" : "password"}
+              className="form-control px-0 auth-input"
+              placeholder="Enter your New Password"
+             {...register('confirmPassword', {
               required: 'confirmation is required!',
               validate: (value) => value === watch('password') || "Passwords do not match!"
             })}
-          />
+            />
+            <span
+              className="input-group-text bg-transparent border-0"
+              onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+              style={{ cursor: "pointer" }}
+            >
+              <FontAwesomeIcon
+                color="#FFF"
+                icon={showNewPassword ? faEyeSlash : faEye}
+              />
+            </span>
+          </div>
         </div>
         {errors.confirmPassword && <span className="text-danger">{errors.confirmPassword.message}</span>}
 
-
-
         <div className="d-flex justify-content-center mt-3">
-          <button className="btn w-75 my-4 text-white py-3 rounded-5" style={{ backgroundColor: '#EF9B28', fontWeight: 500 }}>
-            Verify
+          <button disabled={isLoading} className="btn w-75 my-3 text-white py-3 rounded-5" style={{backgroundColor:'#EF9B28',fontWeight:500}}>
+            {isLoading ? (
+              <>
+                <span
+                  className="spinner-border spinner-border-sm me-2"
+                  role="status"
+                  aria-hidden="true"
+                ></span> Saving
+              </>
+            ) : (
+              "Save"
+            )}
           </button>
         </div>
       </form>
