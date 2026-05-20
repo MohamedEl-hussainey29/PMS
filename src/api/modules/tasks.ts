da91e174351a8c12a87c93e14cc8056687dd79b8
Mohamed Elhussainey <mw61887@gmail.com>
2026-05-20 16:26:53 +0300

    finish tasks-list(get and delete) with delete confirmation modal

--- src/api/modules/tasks.ts
+++ src/api/modules/tasks.ts
@@ -2,6 +2,11 @@
 import type { TasksFormValues } from "../../modules/Tasks/components/TaskData";
 import axiosClient from "../axiosClient";
 
+interface PaginationParams {
+  page: number;
+  size: number;
+}
+
 export const CreateTask = (data:TasksFormValues)=>{
     return axiosClient.post('/task',data);
 }
@@ -10,10 +15,23 @@ export const GetTaskById = (id:number)=>{
     return axiosClient.get(`/task/${id}`);
 }
 
+export const GetTasksByManager = (params?: PaginationParams) => {
+  return axiosClient.get('/task/manager', {
+    params: {
+      pageNumber: params?.page, 
+      pageSize: params?.size    
+    }
+  });
+};
+
 export const UpdateTask = (id:number,data:TasksFormValues)=>{
     return axiosClient.put(`/task/${id}`,data);
 }
 
 export const getTasksCount = () => {
     return axiosClient.get('/Task/count');
+}
+
+export const DeleteTaskById = (id:number) => {
+    return axiosClient.delete(`/Task/${id}`);
 }
