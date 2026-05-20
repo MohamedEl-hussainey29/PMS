7044df07099dc5934dae469c4c0fe02133b68bcc
Mohamed AbdelRaouf <MohamedAbdelRaouff77@gmail.com>
2026-05-20 19:03:22 +0300

    feat: impelement project form

--- src/api/modules/projects.ts
+++ src/api/modules/projects.ts
@@ -1,3 +1,4 @@
+import type { ProjectsFormValues } from "../../modules/Projects/components/ProjectData";
 import axiosClient from "../axiosClient";
 
 interface PaginationParams {
@@ -8,12 +9,23 @@ interface PaginationParams {
 export const GetProjectsByManager = (params?: PaginationParams) => {
   return axiosClient.get('/project/manager', {
     params: {
-      pageNumber: params?.page, 
-      pageSize: params?.size    
+      pageNumber: params?.page,
+      pageSize: params?.size
     }
   });
 };
 
-export const DeleteProjectById = (id:number) => {
-    return axiosClient.delete(`/project/${id}`);
+export const DeleteProjectById = (id: number) => {
+  return axiosClient.delete(`/project/${id}`);
+}
+export const CreateProject = (data: ProjectsFormValues) => {
+  return axiosClient.post('/project', data);
+}
+
+export const GetProjectById = (id: number) => {
+  return axiosClient.get(`/Project/${id}`);
+}
+
+export const UpdateProject = (id: number, data: ProjectsFormValues) => {
+  return axiosClient.put(`/Project/${id}`, data);
 }
